b11ab8f88b04fa54f0976230843ed380f175ad7b
BigMummaHere79 <glenda.fulgidezza@connect.qut.edu.au>
2026-04-20 17:41:52 +1000

    Added primary keys in the models class for testing

--- a2_group2/website/models.py
+++ a2_group2/website/models.py
@@ -3,9 +3,17 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    password_hash = db.Column(db.String(255))
 
 class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
 
 class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255))
 
 class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
