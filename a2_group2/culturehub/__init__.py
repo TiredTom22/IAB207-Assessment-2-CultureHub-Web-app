d4893d971757dd4a4b5117eb2e19be1ea08f76fb
Thompson <thompsontang23@gmail.com>
2026-05-11 14:26:58 +1000

    Minor rename of error handler 500

--- a2_group2/culturehub/__init__.py
+++ a2_group2/culturehub/__init__.py
@@ -39,7 +39,7 @@ def create_app():
         return render_template('errors/404.html'), 404
     
     @app.errorhandler(500)
-    def internal_server_error(e):
+    def internal_error(e):
         return render_template('errors/500.html'), 500
 
     return app
