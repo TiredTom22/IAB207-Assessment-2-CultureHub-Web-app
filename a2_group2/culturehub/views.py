1e9282c0135f27b6d4852fa96c603905049cdcfc
Thompson <thompsontang13@gmail.com>
2026-05-13 19:37:02 +1000

    Added a create event route

--- a2_group2/culturehub/views.py
+++ a2_group2/culturehub/views.py
@@ -19,3 +19,33 @@ def profile():
 @main_bp.route('/edit-profile')
 def edit_profile():
     return render_template('events/edit_profile.html')
+
+@main_bp.route('/event/create', methods=['GET', 'POST'])
+@login_required
+def create_event():
+    form = EventForm()
+    if form.validate_on_submit():
+        # Handle image upload
+        image_file = form.image.data
+        image_filename = secure_filename(image_file.filename)
+        image_file.save(os.path.join('culturehub/static/images', image_filename))
+
+        # Create new event
+        event = Event(
+            name=form.name.data,
+            category=form.category.data,
+            date=form.date.data,
+            location=form.location.data,
+            description=form.description.data,
+            image=image_filename,
+            tickets_available=form.tickets_available.data,
+            price=form.price.data,
+            acknowledgement=form.acknowledgement.data,
+            user_id=current_user.id
+        )
+        db.session.add(event)
+        db.session.commit()
+        flash('Event created successfully!')
+        return redirect(url_for('main.index'))
+    
+    return render_template('events/create.html', form=form)
