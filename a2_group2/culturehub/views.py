5cdcbe898dc22acf03a378d870be3c7bd7b6d79d
Thompson <thompsontang13@gmail.com>
2026-05-13 19:30:38 +1000

    Added new imports

--- a2_group2/culturehub/views.py
+++ a2_group2/culturehub/views.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_required, current_user
+from .forms import EventForm
+from .models import Event
+from . import db
+import os
+from werkzeug.utils import secure_filename
 
 main_bp = Blueprint('main', __name__)
 
