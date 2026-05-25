c468e1c8d10f96b8ebcb39db6ac2158864392359
Sohee Chung <soheejung1200@gmail.com>
2026-05-25 11:14:40 +1000

    Added multiple files

--- a2_group2/culturehub/views.py
+++ a2_group2/culturehub/views.py
@@ -23,7 +23,6 @@ def profile():
 
 @main_bp.route('/user/edit-profile')
 def edit_profile():
-<<<<<<< Updated upstream
     return render_template('user/edit_profile.html')
 
 @main_bp.route('/event/create', methods=['GET', 'POST'])
@@ -83,14 +82,18 @@ def add_comment(event_id):
     db.session.add(comment)
     db.session.commit()
     return redirect(url_for('main.event_detail', event_id=event_id) + '#comments')
-=======
-    return render_template('events/edit_profile.html')
 
-@main_bp.route('/create_event')
-def create_event():
-    return render_template('events/create_event.html')
 
-@main_bp.route('/comment')
-def comment():
-    return render_template('events/comment.html')
->>>>>>> Stashed changes
+@main_bp.route('/about-us')
+def about_us():
+    return render_template('community/about_us.html')
+
+
+@main_bp.route('/stories')
+def stories():
+    return render_template('community/stories.html')
+
+
+@main_bp.route('/contact')
+def contact():
+    return render_template('community/contact.html')
